86b042696935572b302cbee0b0524526251ab35b
Diego Serrat <31071378+diegoserrat@users.noreply.github.com>
2025-09-05 19:19:31 -0300

    fix to show the right icon on list

--- src/app/shared/utils/helper/icon.helper.ts
+++ src/app/shared/utils/helper/icon.helper.ts
@@ -3,9 +3,9 @@ import { ListType } from "../../types/list";
 export const iconHelper = (type: ListType) => {
     switch (type) {
         case ListType.LUXO:
-            return 'shopping_cart';
-        case ListType.ALIMENTACAO:
             return 'diamond_shone';
+        case ListType.ALIMENTACAO:
+            return 'shopping_cart';
         case ListType.CONTAS_FIXAS:
             return 'wallet';
     }
